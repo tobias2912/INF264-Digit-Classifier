399262485a9eeb19fbd887407a72ed05ba9ac621
Tobias <tseilertsen@gmail.com>
2020-10-23 17:04:21 +0200

    changed to class based

--- src/main.py
+++ src/main.py
@@ -12,11 +12,44 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy import genfromtxt
 
-def get_feature(fileName):
-    return genfromtxt(fileName, delimiter=',')
+def main():
+    '''
+    loads dataset and learns 3 models. chooses best model and hyperpatrameters
+    with grid search and cross validation.
+    calculates accuracy of selected model on test set.
+    '''
+    np.random.seed(123456)
+    if False:
+        #create a smaller testfile for testing
+        digits = get_feature('../data/handwritten_digits_images.csv')
+        label = get_label('../data/handwritten_digits_labels.csv')
+        create_smaller_file(digits, label)
+    else:
+        #read given file
+        digits=get_feature('../data/digit_smaller.csv')
+        label=get_feature('../data/label_smaller.csv')
+    
+    X_train, Y_train, X_test, Y_test = split(digits, label)
 
-def get_label(fileName):
-    return genfromtxt(fileName, delimiter='\n')
+    Y_train = Y_train.reshape(-1,1)
+    Y_test = Y_test.reshape(-1,1)
+    # train and test different classifiers
+
+    X_train =  preprocessing.normalize(X_train) 
+    X_test =  preprocessing.normalize(X_test) 
+
+    # baggingkneighbors
+    classifiers = [randomforest(), support_vector() ,kneighbors()]
+    best_clf = None
+    best_score = 0
+    for clf in classifiers:
+        clf.fit(X_train, Y_train)
+        score = clf.get_grid_search().best_score_
+        if score>best_score:
+            best_clf = clf
+    print('best classifier was ', best_clf)
+    #perform prediction with best classifier
+
 
 def create_smaller_file(X, y):
     '''create and save a smaller file for testing'''
@@ -36,7 +69,6 @@ def split(digits, label):
                                                                 test_size=0.3, 
                                                                 shuffle=True, 
                                                                 random_state=seed)
-
     return X_train, Y_train, X_test, Y_test
        
 
@@ -57,8 +89,8 @@ def plot(digits, label, predict):
     print(label[predict])
     plt.show()    
 
-def print_stats(clf, X_test, Y_test):
-    '''prints stats on classifier performance'''
+def model_stats(clf, X_test, Y_test):
+    '''predicts test valuesprints stats on classifier performance'''
     print("best params: ", clf.best_params_)
     print("predicts test data...")
     predict = clf.predict(X_test)
@@ -66,6 +98,7 @@ def print_stats(clf, X_test, Y_test):
     disp = metrics.plot_confusion_matrix(clf, X_test, Y_test)
     print("Confusion matrix:\n%s" % disp.confusion_matrix)
     print('score ',score)
+    return score
 
 def randomforest(X_train, Y_train, X_test, Y_test):
     '''Create a randomforest model with cross validation parameter search'''
@@ -74,7 +107,8 @@ def randomforest(X_train, Y_train, X_test, Y_test):
     print("fits classifier...")
     clf = GridSearchCV(classifier, tuned_parameters)
     clf.fit(X_train, Y_train.ravel())
-    print_stats(clf, X_test, Y_test)
+    print(clf.best_score_)   
+    return model_stats(clf, X_test, Y_test)
 
 def kneighbors(X_train, Y_train, X_test, Y_test):
     '''Create a K-neighbors model with cross validation parameter search'''
@@ -83,7 +117,7 @@ def kneighbors(X_train, Y_train, X_test, Y_test):
     print("fits classifier...")
     clf = GridSearchCV(classifier, tuned_parameters)
     clf.fit(X_train, Y_train.ravel())
-    print_stats(clf, X_test, Y_test)
+    return model_stats(clf, X_test, Y_test)
 
 def baggingkneighbors(X_train, Y_train, X_test, Y_test):
     '''Create a baggingkneighbors model with cross validation parameter search'''
@@ -92,7 +126,8 @@ def baggingkneighbors(X_train, Y_train, X_test, Y_test):
     print("fits classifier...")
     clf = GridSearchCV(classifier, tuned_parameters)
     clf.fit(X_train, Y_train.ravel())
-    print_stats(clf, X_test, Y_test)
+    print(clf.best_score_)
+    return model_stats(clf, X_test, Y_test)
 
 def support_vector(X_train, Y_train, X_test, Y_test):
     '''Create a K-neighbors model with cross validation parameter search'''
@@ -101,30 +136,13 @@ def support_vector(X_train, Y_train, X_test, Y_test):
     print("fits classifier...")
     clf = GridSearchCV(classifier, tuned_parameters)
     clf.fit(X_train, Y_train.ravel())
-    print_stats(clf, X_test, Y_test)
+    return model_stats(clf, X_test, Y_test)
+
+def get_feature(fileName):
+    return genfromtxt(fileName, delimiter=',')
+
+def get_label(fileName):
+    return genfromtxt(fileName, delimiter='\n')
 
 if __name__ == "__main__":    
-    np.random.seed(123456)
-    if False:
-        #create a smaller testfile for testing
-        digits = get_feature('../data/handwritten_digits_images.csv')
-        label = get_label('../data/handwritten_digits_labels.csv')
-        create_smaller_file(digits, label)
-    else:
-        #read given file
-        digits=get_feature('../data/digit_smaller.csv')
-        label=get_feature('../data/label_smaller.csv')
-    
-    X_train, Y_train, X_test, Y_test = split(digits, label)
-
-    Y_train = Y_train.reshape(-1,1)
-    Y_test = Y_test.reshape(-1,1)
-    # train and test different classifiers
-
-    X_train =  preprocessing.normalize(X_train) 
-    X_test =  preprocessing.normalize(X_test) 
-    baggingkneighbors(X_train, Y_train, X_test, Y_test)
-    randomforest(X_train, Y_train, X_test, Y_test)
-    support_vector(X_train, Y_train, X_test, Y_test)
-    kneighbors(X_train, Y_train, X_test, Y_test)
-    
+    main()
